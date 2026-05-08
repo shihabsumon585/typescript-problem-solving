54400b162e042c2095b195cfe30c8df369a06a7f
Md Shihab Sumon <shihabsumon585@gmail.com>
2026-05-08 16:39:47 +0600

    problem - 2 solve

--- typescript.ts
+++ typescript.ts
@@ -18,3 +18,23 @@ const calculateTotal = ({ price, quantity = 1 }: CartItem): number => {
 // console.log(calculateTotal(cartItem));
 
 
+// problem 2
+type Person = {
+    name: string;
+    age: number;
+}
+type JobDetails = {
+    role: string;
+    sallary: number;
+}
+type Employee = Person & JobDetails;
+const employee : Employee = {
+    name: "Md Shihab Sumon",
+    age: 23,
+    role: "Next Level Developer",
+    sallary: 25000
+}
+const getProfile = ({name, role}: Employee): string => {
+    return `Name: ${name}, Role: ${role}`
+}
+// console.log(getProfile(employee));
